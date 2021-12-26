b6533af3db3a7c5f6d714759361df90024095ba7
Albert <albert.barsamov@gmail.com>
2021-12-26 23:29:48 +0100

    Clocks: index.js - refatoring

--- Clocks/index.js
+++ Clocks/index.js
@@ -1,6 +1,5 @@
-
+// Change type of Clock
 const btn = document.querySelector('.btn')
-// const digitalBtn = document.querySelector('.digit_btn')
 const clockSlider = document.querySelector('.clock_slider')
 
 btn.addEventListener('click', () => {
@@ -11,7 +10,7 @@ btn.addEventListener('click', () => {
 })
 
 
-
+// Analog clock 
 function analog_clock() {
 
     const hourArrow = document.querySelector('.analog_hours');
@@ -19,15 +18,19 @@ function analog_clock() {
     const secondeArrow = document.querySelector('.analog_secondes');
 
     const date = new Date()
-
+    // 6 => degree of every seconde (360/60 => 6°)
     const secondes = date.getSeconds()
     const secondesDeg = secondes * 6
     secondeArrow.style.transform = `rotateZ(${secondesDeg}deg)`
-
+    // 6 => same as seconde
     const minutes = date.getMinutes()
     const minutesDeg = minutes * 6
     minuteArrow.style.transform = `rotateZ(${minutesDeg}deg)`
 
+    // 30 => degree of hour (360/ 12 => 30°)
+    // hoursDeg + (minutesDeg / 12) => to move Hour hand with minutes
+    // => 2h 54min => 60° + 4,5° => 64,5°
+    // => 2h 57min => 60° + 4,75° => 64,75° etc
     const hours = date.getHours()
     const hoursDeg = hours * 30
     hourArrow.style.transform = `rotateZ(${hoursDeg + (minutesDeg / 12)}deg)`
@@ -35,9 +38,9 @@ function analog_clock() {
 }
 
 setInterval(analog_clock, 1000)
-
 analog_clock()
 
+// Digital clock
 function digital_clock() {
     const date = new Date()
     const hoursScreen = document.querySelector('.digital_hours')
@@ -46,6 +49,7 @@ function digital_clock() {
 
     const secondes = date.getSeconds()
 
+    // if it <= 9 add "0" => 09 etc.
     secondes <= 9
         ? secondesScreen.innerHTML = `0${secondes}`
         : secondesScreen.innerHTML = secondes
